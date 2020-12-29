2c66edea3c5782c1e91710b7ee4bdc07a7c25c01
Guilherme Noguchi <8411808@educacao.intranet>
2020-12-29 15:59:02 -0300

    iniciado o uso de rotas

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container" id="app">
+    <div class="container">
+      <FrameSeletorTemplateEmail v-model="templateEmailSelecionado" v-on:input="limpaModeloEmail"></FrameSeletorTemplateEmail>
+    </div>
+    <div class="container">
+      <router-view></router-view>
+      <FrameTemplateEmailDns v-if="templateEmailSelecionado==1" v-model="modeloEmail"></FrameTemplateEmailDns>
+      <FrameTemplateEmailFirewall v-if="templateEmailSelecionado==2" v-model="modeloEmail"></FrameTemplateEmailFirewall>
+      <FrameTemplateEmailResultadoDiligencia v-if="templateEmailSelecionado==3" v-model="modeloEmail"></FrameTemplateEmailResultadoDiligencia>
+      <FrameTemplateEmailConviteGithub v-if="templateEmailSelecionado==4" v-model="modeloEmail"></FrameTemplateEmailConviteGithub>
+    </div>
+    <div class="container">
+      <FrameEmailGerado v-if="modeloEmail" :modeloEmail="modeloEmail"></FrameEmailGerado>
+    </div>
+  </div>
+</template>
+
+<script>
+import FrameSeletorTemplateEmail from '@/components/FrameSeletorTemplateEmail.vue'
+import FrameTemplateEmailDns from '@/components/FrameTemplateEmailDns.vue'
+import FrameTemplateEmailFirewall from '@/components/FrameTemplateEmailFirewall.vue'
+import FrameTemplateEmailResultadoDiligencia from '@/components/FrameTemplateEmailResultadoDiligencia.vue'
+import FrameTemplateEmailConviteGithub from '@/components/FrameTemplateEmailConviteGithub.vue'
+import FrameEmailGerado from '@/components/FrameEmailGerado.vue'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      templateEmailSelecionado: null,
+      modeloEmail: null
+    }
+  },
+  components: {
+    FrameSeletorTemplateEmail,
+    FrameTemplateEmailDns,
+    FrameTemplateEmailFirewall,
+    FrameTemplateEmailResultadoDiligencia,
+    FrameTemplateEmailConviteGithub,
+    FrameEmailGerado
+  },
+  methods: {
+    limpaModeloEmail: function () {
+      this.modeloEmail = null
+    }
+  }
+}
+</script>
---
+++ src/views/TemplateEmailDns.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <FrameTemplateEmailDns v-model="modeloEmail"></FrameTemplateEmailDns>
+    </div>
+</template>
+
+<script>
+import FrameTemplateEmailDns from '@/components/FrameTemplateEmailDns.vue'
+
+export default {
+    components: {
+        FrameTemplateEmailDns
+    }
+}
+</script>
