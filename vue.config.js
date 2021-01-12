572b6997666fcdf3b111a95afcd3a60508a4950e
Guilherme Noguchi <8411808@educacao.intranet>
2021-01-12 11:58:44 -0300

    atualizado o docs

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-	//publicPath: "/template-de-email/",
-	publicPath: "", //débito técnico arrumar baseado na variável de ambiente "NODE_ENV"
+	publicPath: "/template-de-email/",
 	outputDir: "docs"
 };
