869c48895fa109343da1ded816432e0f120df5e7
Guilherme Noguchi <8411808@educacao.intranet>
2021-01-12 11:55:44 -0300

    alinhado o email gerado

--- src/components/FrameEmailGerado.vue
+++ src/components/FrameEmailGerado.vue
@@ -1,36 +1,54 @@
 <template>
-    <div>
-        <div>
-            {{ titulo }}
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                {{ titulo }}
+            </div>
         </div>
-        <label>
-            Para:
-            <div>
+        <div class="row">
+            <div class="col-1">
+                <label for="emailSuporte">Para:</label>
+            </div>
+            <div id="emailSuporte" class="col-11">
                 {{ emailSuporte }}
             </div>
-        </label>
-        <label>
-            CC:
-            <div>
-                {{ emailInfraestruturaCentralIT }}
+        </div>
+        <div class="row">
+            <div class="col-1">
+                <label for="emailCopia">CC:</label>
             </div>
-            <div>
-                {{ emailDISIS }}
+            <div id="emailCopia" class="col-11">
+                <div class="row">
+                    <div class="col">
+                        {{ emailInfraestruturaCentralIT }}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        {{ emailDISIS }}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        {{ emailSolicitante }}
+                    </div>
+                </div>
             </div>
-            <div>
-                {{ emailSolicitante }}
+        </div>
+        <div class="row">
+            <div class="col-1">
+                <label for="assunto">Assunto:</label>
             </div>
-        </label>
-        <label>
-            Assunto:
-            <div v-html="modeloEmail.assunto">
+            <div id="assunto" class="col-11" v-html="modeloEmail.assunto">
             </div>
-        </label>
-        <label>
-            Corpo:
-            <div v-html="modeloEmail.corpo">
+        </div>
+        <div class="row">
+            <div class="col-1">
+                <label for="corpo">Corpo:</label>
             </div>
-        </label>
+            <div id="corpo" class="col-11" v-html="modeloEmail.corpo">
+            </div>
+        </div>
     </div>
 </template>
 
